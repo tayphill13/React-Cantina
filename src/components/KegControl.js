c6e6af64394f76c2b11ff290ba9a3b467477ae42
Taylor Phillips <taylorphillips133@gmail.com>
2020-09-11 10:58:49 -0700

    complete the KegControl file that determines what state to display and put in code for possible update feature

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -13,8 +13,32 @@ class KegControl extends React.Component  {
     };
   }
 
-
-
-
+  render(){
+    let currentlyVisibleState = null;
+    if (this.state.currentPage === 'newKegForm')  {
+      currentlyVisibleState = <NewKegForm
+                                  onNewKegCreation={this.handleAddingNewKegToList}
+                                  onClick = {this.handleClick}
+                                  update = {false} />
+    } else if (this.state.currentPage === 'list') {
+      currentlyVisibleState = <KegList
+                                  kegList = {this.state.masterKegList}
+                                  onClick = {this.handleClick}
+                                  onUpdateClick = {this.handleUpdateClick} />
+    } else if (this.state.currentPage === 'updateKegForm')  {
+      currentlyVisibleState = <NewKegForm
+                                  onUpdateKeg = {this.handleUpdateKeg}
+                                  onClick = {this.handleClick}
+                                  update = {true}
+                                  kegId = {this.state.currentKegId} />
+    }
+    return (
+      <React.Fragment>
+        {currentlyVisibleState}
+      </React.Fragment>
+    );
+  }
 
 }
+
+export default KegControl;
---
+++ src/components/KegList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Keg from "./Keg";
+import PropTypes from "prop-types";
+
+function KegList(props) {
+  return (
+    <React.Fragment>
+      <hr />
+      {props.kegList.map((keg) =>
+      <Keg
+        name={keg.name}
+        brand={keg.brand}
+        price={keg.price}
+        abv={keg.abv}
+        style={keg.style}
+        pintsRemaining={keg.pintsRemaining}
+        kegId={keg.id}
+        key={keg.id} />
+    
+      )}
+      <button onClick={props.onClick}>Add Keg</button>
+    </React.Fragment>
+  );
+}
+
+KegList.propTypes = {
+  kegList: PropTypes.array,
+  onClick: PropTypes.func
+};
+
+
+export default KegList;
