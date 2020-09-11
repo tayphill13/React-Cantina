5c82315d9daf713eb6d9272968ecdd801bf9a0a1
Taylor Phillips <taylorphillips133@gmail.com>
2020-09-11 10:38:02 -0700

    add constructor to the kegcontrol, before adding any event handlers

---
+++ src/components/KegControl.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import NewKegForm from './NewKegForm';
+import KegList from './KegList';
+
+class KegControl extends React.Component  {
+
+  constructor(props)  {
+    super(props);
+    this.state = {
+      currentPage: "list",
+      masterKegList: [],
+      currentKegId: null
+    };
+  }
+
+
+
+
+
+}
