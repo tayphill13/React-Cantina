e9d511222bcb19f70e104e80dd51221c4657598b
Taylor Phillips <taylorphillips133@gmail.com>
2020-09-11 16:13:57 -0700

    add bootstrap form setup to my reusable keg form--

--- src/components/KegForm.js
+++ src/components/KegForm.js
@@ -1,18 +1,65 @@
 import React from 'react';
-import { v4 } from 'uuid';
 import PropTypes from 'prop-types';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
 
-function NewKegForm(props) {
-  const { onLinkClick, onSubmittingForm, buttonText, defaultName, defaultBrand, defaultPrice, defaultOrigin, defaultPintsRemaining } = props;
+function KegForm(props) {
+  const { onClick, onSubmittingForm, buttonText, defaultName, defaultBrand, defaultPrice, defaultOrigin, defaultPintsRemaining } = props;
 
-  function handleKegFormSubmission(props) {
+  function handleFormSubmission(event) {
     event.preventDefault();
     onSubmittingForm(event);
   }
 
+  return (
+    <React.Fragment>
+      <Form onSubmit={handleFormSubmission}>
+        <Form.Group controlId="name">
+          <Form.Label>Beverage</Form.Label>
+          <Form.Control type='text' placeholder="Beverage Name" defaultValue={defaultName} required />
+        </Form.Group>
+        <Form.Group controlId="price">
+          <Form.Label>Price</Form.Label>
+          <InputGroup>
+            <InputGroup.Prepend>
+              <InputGroup.Text>$</InputGroup.Text>
+            </InputGroup.Prepend>
+            <Form.Control type='text' placeholder='0.00' defaultValue={defaultPrice} pattern='^\d+\.\d{2}$' required />
+          </InputGroup>
+          <Form.Text className='text-muted'>Format as a decimal (e.g. 3.99 or 12.49)</Form.Text>
+        </Form.Group>
+        <Form.Group controlId="pintsRemaining">
+          <Form.Label>Pints Remaining</Form.Label>
+          <Form.Control type='number' placeholder='124' defaultValue={defaultPintsRemaining} required />
+        </Form.Group>
+        <Form.Group controlId="brand">
+          <Form.Label>Brand</Form.Label>
+          <Form.Control type='text' placeholder="Beverage Brand" defaultValue={defaultBrand} required />
+        </Form.Group>
+        <Form.Group controlId='origin'>
+          <Form.Label>Origin</Form.Label>
+          <Form.Control type='text' placeholder="Planet of Origin" defaultValue={defaultOrigin} required />
+        </Form.Group>
+        <Button className='mb-3' variant='success' type="submit" size='lg' >{buttonText}</Button>
+      </Form>
+      <Button className='mb-3' variant='secondary' type='button' size='lg' onClick={()=>onClick('list')}>Back to Keg List</Button>
+    </React.Fragment>
+  )
+}
 
+KegForm.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  onSubmittingForm: PropTypes.func.isRequired,
+  buttonText: PropTypes.string.isRequired,
+  defaultName: PropTypes.string,
+  defaultPrice: PropTypes.string,
+  defaultBrand: PropTypes.string,
+  defaultOrigin: PropTypes.string,
+  defaultPintsRemaining: PropTypes.number
+}
 
-
+export default KegForm;
 
 
 
@@ -71,7 +118,7 @@ function NewKegForm(props) {
 //     </React.Fragment>
 //   );
 
-// }
+// 
 
 // NewKegForm.propTypes = {
 //   onNewKegCreation: PropTypes.func,
